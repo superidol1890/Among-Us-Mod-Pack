9096ddc228f41c8981cd8bbb8bb67d09e546b842
Super Idol <viyanshkakkar@gmail.com>
2025-03-31 16:13:00 +0530

    Add files via upload

---
+++ Town Of Host/Town Of Host/Patches/MainMenuManagerPatch.cs
@@ -0,0 +1,142 @@
+using System;
+using HarmonyLib;
+using TownOfHost.Templates;
+using UnityEngine;
+
+namespace TownOfHost
+{
+    [HarmonyPatch(typeof(MainMenuManager))]
+    public class MainMenuManagerPatch
+    {
+        private static SimpleButton discordButton;
+        public static SimpleButton UpdateButton { get; private set; }
+        private static SimpleButton gitHubButton;
+
+        [HarmonyPatch(nameof(MainMenuManager.Start)), HarmonyPostfix, HarmonyPriority(Priority.Normal)]
+        public static void StartPostfix(MainMenuManager __instance)
+        {
+            SimpleButton.SetBase(__instance.quitButton);
+            //Discordボタンを生成
+            if (SimpleButton.IsNullOrDestroyed(discordButton))
+            {
+                discordButton = CreateButton(
+                    "DiscordButton",
+                    new(-1f, -1f, 1f),
+                    new(88, 101, 242, byte.MaxValue),
+                    new(148, 161, byte.MaxValue, byte.MaxValue),
+                    () => Application.OpenURL(Main.DiscordInviteUrl),
+                    "Discord",
+                    isActive: Main.ShowDiscordButton);
+            }
+
+            // GitHubボタンを生成
+            if (SimpleButton.IsNullOrDestroyed(gitHubButton))
+            {
+                gitHubButton = CreateButton(
+                    "GitHubButton",
+                    new(1f, -1f, 1f),
+                    new(153, 153, 153, byte.MaxValue),
+                    new(209, 209, 209, byte.MaxValue),
+                    () => Application.OpenURL("https://github.com/tukasa0001/TownOfHost"),
+                    "GitHub");
+            }
+
+            //Updateボタンを生成
+            if (SimpleButton.IsNullOrDestroyed(UpdateButton))
+            {
+                UpdateButton = CreateButton(
+                    "UpdateButton",
+                    new(0f, -1.7f, 1f),
+                    new(0, 202, 255, byte.MaxValue),
+                    new(60, 255, 255, byte.MaxValue),
+                    () =>
+                    {
+                        UpdateButton.Button.gameObject.SetActive(false);
+                        ModUpdater.StartUpdate(ModUpdater.downloadUrl);
+                    },
+                    $"{Translator.GetString("updateButton")}\n{ModUpdater.latestTitle}",
+                    new(2.5f, 1f),
+                    isActive: false);
+            }
+
+#if RELEASE
+            // フリープレイの無効化
+            var howToPlayButton = __instance.howToPlayButton;
+            var freeplayButton = howToPlayButton.transform.parent.Find("FreePlayButton");
+            if (freeplayButton != null)
+            {
+                freeplayButton.gameObject.SetActive(false);
+            }
+            // フリープレイが消えるのでHowToPlayをセンタリング
+            howToPlayButton.transform.SetLocalX(0);
+#endif
+        }
+
+        /// <summary>TOHロゴの子としてボタンを生成</summary>
+        /// <param name="name">オブジェクト名</param>
+        /// <param name="normalColor">普段のボタンの色</param>
+        /// <param name="hoverColor">マウスが乗っているときのボタンの色</param>
+        /// <param name="action">押したときに発火するアクション</param>
+        /// <param name="label">ボタンのテキスト</param>
+        /// <param name="scale">ボタンのサイズ 変更しないなら不要</param>
+        private static SimpleButton CreateButton(
+            string name,
+            Vector3 localPosition,
+            Color32 normalColor,
+            Color32 hoverColor,
+            Action action,
+            string label,
+            Vector2? scale = null,
+            bool isActive = true)
+        {
+            var button = new SimpleButton(CredentialsPatch.TohLogo.transform, name, localPosition, normalColor, hoverColor, action, label, isActive);
+            if (scale.HasValue)
+            {
+                button.Scale = scale.Value;
+            }
+            return button;
+        }
+
+        // プレイメニュー，アカウントメニュー，クレジット画面が開かれたらロゴとボタンを消す
+        [HarmonyPatch(nameof(MainMenuManager.OpenGameModeMenu))]
+        [HarmonyPatch(nameof(MainMenuManager.OpenOnlineMenu))]
+        [HarmonyPatch(nameof(MainMenuManager.OpenEnterCodeMenu))]
+        [HarmonyPatch(nameof(MainMenuManager.ClickBackOnline))]
+        [HarmonyPatch(nameof(MainMenuManager.OpenAccountMenu))]
+        [HarmonyPatch(nameof(MainMenuManager.OpenCredits))]
+        [HarmonyPostfix]
+        public static void OpenMenuPostfix()
+        {
+            if (CredentialsPatch.TohLogo != null)
+            {
+                CredentialsPatch.TohLogo.gameObject.SetActive(false);
+            }
+        }
+        [HarmonyPatch(nameof(MainMenuManager.ResetScreen)), HarmonyPostfix]
+        public static void ResetScreenPostfix()
+        {
+            if (CredentialsPatch.TohLogo != null)
+            {
+                CredentialsPatch.TohLogo.gameObject.SetActive(true);
+            }
+        }
+        [HarmonyPatch(nameof(MainMenuManager.OpenOnlineMenu)), HarmonyPostfix]
+        public static void FindGameButtonDisable(MainMenuManager __instance)
+        {
+            __instance.findGameButton.gameObject.SetActive(false);
+            //中央Lineを消す
+            var line = __instance.findGameButton.gameObject.transform.parent.Find("Line");
+            if (line != null)
+            {
+                line.gameObject.SetActive(false);
+            }
+            //ロビー作成、コード入力ボタンを中央に移動
+            __instance.createGameButton.transform.SetLocalX(0);
+            var enterCodeButton = __instance.createGameButton.transform.parent.Find("Enter Code Button");
+            if (enterCodeButton != null)
+            {
+                enterCodeButton.gameObject.transform.SetLocalX(0);
+            }
+        }
+    }
+}
