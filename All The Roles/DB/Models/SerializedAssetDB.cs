2d0d662e27f29299ac27a99d309170015d87e43a
Super Idol <viyanshkakkar@gmail.com>
2025-04-24 18:11:04 +0530

    Add files via upload

---
+++ All The Roles/DB/Models/SerializedAssetDB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LevelImposter.DB;
+
+[Serializable]
+public class SerializedAssetDB
+{
+    public List<ObjectDB.DBElement> ObjectDB { get; set; }
+    public List<TaskDB.DBElement> TaskDB { get; set; }
+    public List<SoundDB.DBElement> SoundDB { get; set; }
+    public List<PathDB.DBElement> PathDB { get; set; }
+}
