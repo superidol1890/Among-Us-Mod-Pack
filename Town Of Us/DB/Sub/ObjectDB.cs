d268a77bdc33c3ca314ef3f63ba5e1b387cc5250
Super Idol <viyanshkakkar@gmail.com>
2025-04-27 18:30:58 +0530

    Add files via upload

---
+++ Town Of Us/DB/Sub/ObjectDB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using LevelImposter.Core;
+using UnityEngine;
+
+namespace LevelImposter.DB;
+
+/// <summary>
+///     Database of Among Us GameObjects
+/// </summary>
+public class ObjectDB(SerializedAssetDB serializedDB) : SubDB<GameObject>(serializedDB)
+{
+    public override void LoadShip(ShipStatus shipStatus, MapType mapType)
+    {
+        DB.ObjectDB.ForEach(elem =>
+        {
+            if (mapType != elem.MapType)
+                return;
+
+            // Transform
+            var transform = shipStatus.transform.Find(elem.Path);
+            if (transform == null)
+            {
+                LILogger.Warn($"ObjectDB could not find {elem.ID} in {shipStatus.name}");
+                return;
+            }
+
+            Add(elem.ID, transform.gameObject);
+        });
+    }
+
+    [Serializable]
+    public class DBElement
+    {
+        public string ID { get; set; }
+        public string Path { get; set; }
+        public int Map { get; set; }
+
+        [JsonIgnore] public MapType MapType => (MapType)Map;
+    }
+}
---
+++ Town Of Us/DB/Sub/SubDB.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using LevelImposter.Core;
+
+namespace LevelImposter.DB;
+
+/// <summary>
+///     A miniature DB designed to search and
+///     contain various types of objects within Among Us
+/// </summary>
+/// <typeparam name="T">Type of object to contain</typeparam>
+public abstract class SubDB<T>(SerializedAssetDB serializedDB)
+{
+    private readonly Dictionary<string, T> _data = new();
+
+    protected SerializedAssetDB DB { get; } = serializedDB;
+
+    /// <summary>
+    ///     Loads any extra assets into the DB
+    /// </summary>
+    public virtual void Load()
+    {
+    }
+
+    /// <summary>
+    ///     Loads each ship into the DB
+    /// </summary>
+    /// <param name="shipStatus">ShipStatus to load</param>
+    /// <param name="mapType">MapType of ShipStatus</param>
+    public virtual void LoadShip(ShipStatus shipStatus, MapType mapType)
+    {
+    }
+
+    /// <summary>
+    ///     Gets an object from the DB
+    /// </summary>
+    /// <param name="id">Type ID of the object</param>
+    /// <returns>Object or null if not found</returns>
+    public T? Get(string id)
+    {
+        _data.TryGetValue(id, out var result);
+        return result;
+    }
+
+    /// <summary>
+    ///     Adds an object to the DB
+    /// </summary>
+    /// <param name="id">ID of the object</param>
+    /// <param name="obj">Object to add</param>
+    protected void Add(string id, T obj)
+    {
+        _data.Add(id, obj);
+    }
+}
