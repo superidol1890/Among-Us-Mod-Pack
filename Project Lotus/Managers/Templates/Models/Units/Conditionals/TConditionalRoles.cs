282af5336fa43c4832ef7703f49afaa4ef5fc07e
Super Idol <viyanshkakkar@gmail.com>
2025-05-13 19:07:41 +0530

    Add files via upload

---
+++ Project Lotus/Managers/Templates/Models/Units/Conditionals/TConditionalRoles.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Lotus.Extensions;
+using Lotus.Roles;
+
+namespace Lotus.Managers.Templates.Models.Units.Conditionals;
+
+public class TConditionalRoles : StringListConditionalUnit
+{
+    private HashSet<string>? rolesLower;
+
+    public TConditionalRoles(object input) : base(input)
+    {
+    }
+
+    public override bool Evaluate(object? data)
+    {
+        return data is not PlayerControl player || VerifyRole(player);
+    }
+
+    public bool VerifyRole(PlayerControl? player)
+    {
+        if (player == null) return true;
+        rolesLower ??= Values.Select(r => r.ToLower()).ToHashSet();
+        return player.GetAllRoleDefinitions().Any(VerifyRole);
+    }
+
+    public bool VerifyRole(CustomRole roleDefinition)
+    {
+        return rolesLower == null || rolesLower.Contains(roleDefinition.RoleName);
+    }
+}
