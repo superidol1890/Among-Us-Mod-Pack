0ae1e679f0f50d717ca029ebbebc9f3d4a776c50
Super Idol <viyanshkakkar@gmail.com>
2025-05-15 17:39:04 +0530

    Add files via upload

---
+++ Endless Host Roles/Roles/Impostor/Roles/AddOns/Impostor/Taskcounter.cs
@@ -0,0 +1,11 @@
+﻿namespace EHR.AddOns.Impostor;
+
+internal class Taskcounter : IAddon
+{
+    public AddonTypes Type => AddonTypes.ImpOnly;
+
+    public void SetupCustomOption()
+    {
+        Options.SetupAdtRoleOptions(14370, CustomRoles.Taskcounter, canSetNum: true);
+    }
+}
---
+++ Endless Host Roles/Roles/Impostor/Roles/IVanillaSettingHolder.cs
@@ -0,0 +1,7 @@
+﻿namespace EHR;
+
+internal interface IVanillaSettingHolder
+{
+    public TabGroup Tab { get; }
+    public void SetupCustomOption();
+}
