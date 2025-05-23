ebd771629f231377389185f67e9c91a7dbb13254
Super Idol <viyanshkakkar@gmail.com>
2025-05-23 14:15:44 +0530

    Add files via upload

---
+++ Endless Host Roles/Roles/Neutral/Pawn.cs
@@ -0,0 +1,35 @@
+﻿namespace EHR.Neutral;
+
+public class Pawn : RoleBase
+{
+    public static bool On;
+
+    public CustomRoles ChosenRole;
+
+    public override bool IsEnable => On;
+
+    public override void SetupCustomOption()
+    {
+        StartSetup(651500)
+            .CreateOverrideTasksData();
+    }
+
+    public override void Init()
+    {
+        On = false;
+    }
+
+    public override void Add(byte playerId)
+    {
+        On = true;
+        ChosenRole = CustomRoles.NotAssigned;
+    }
+
+    public override void OnFixedUpdate(PlayerControl pc)
+    {
+        if (!pc.AllTasksCompleted() || ChosenRole == CustomRoles.NotAssigned || ChosenRole.IsAdditionRole() || ChosenRole.IsForOtherGameMode()) return;
+
+        pc.RpcSetCustomRole(ChosenRole);
+        pc.RpcChangeRoleBasis(ChosenRole);
+    }
+}
