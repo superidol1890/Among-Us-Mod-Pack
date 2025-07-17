cfdc1d1199c0aadd5bc6cfaf3b945b738d3825cd
Super Idol <viyanshkakkar@gmail.com>
2025-07-17 15:37:42 +0530

    Add files via upload

---
+++ Endless Host Roles/Roles/AddOns/Common/Magnet.cs
@@ -0,0 +1,11 @@
+﻿namespace EHR.AddOns.Common;
+
+internal class Magnet : IAddon
+{
+    public AddonTypes Type => AddonTypes.Helpful;
+
+    public void SetupCustomOption()
+    {
+        Options.SetupAdtRoleOptions(19692, CustomRoles.Magnet, canSetNum: true, teamSpawnOptions: true);
+    }
+}
---
+++ Endless Host Roles/Roles/AddOns/IAddon.cs
@@ -0,0 +1,7 @@
+﻿namespace EHR.AddOns;
+
+internal interface IAddon
+{
+    public AddonTypes Type { get; }
+    public void SetupCustomOption();
+}
