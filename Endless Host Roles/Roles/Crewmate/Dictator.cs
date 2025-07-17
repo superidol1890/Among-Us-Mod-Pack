6a4feb88813bbe6cb6ac030b0412c00a7ae031da
Super Idol <viyanshkakkar@gmail.com>
2025-07-17 15:50:32 +0530

    Add files via upload

---
+++ Endless Host Roles/Roles/Crewmate/Dictator.cs
@@ -0,0 +1,15 @@
+﻿namespace EHR.Crewmate;
+
+internal class Dictator : RoleBase
+{
+    public override bool IsEnable => false;
+
+    public override void SetupCustomOption()
+    {
+        Options.SetupRoleOptions(652400, TabGroup.CrewmateRoles, CustomRoles.Dictator);
+    }
+
+    public override void Init() { }
+
+    public override void Add(byte playerId) { }
+}
