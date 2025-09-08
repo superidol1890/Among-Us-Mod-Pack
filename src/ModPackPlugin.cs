d5886d501f48c8bd6ca515c945211d01685363c6
Super Idol <viyanshkakkar@gmail.com>
2025-09-08 18:32:55 +0530

    Added Mod source code and also about the mod

---
+++ src/ModPackPlugin.cs
@@ -0,0 +1,102 @@
+using System;
+using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using Il2CppInterop.Runtime.Attributes;
+using Reactor.Localization;
+using Reactor.Localization.Utilities;
+using Reactor.Networking;
+using Reactor.Networking.Attributes;
+using Reactor.Networking.Rpc;
+using Reactor.Utilities;
+using Reactor.Utilities.Attributes;
+using Reactor.Utilities.ImGui;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace ModPack;
+
+[BepInAutoPlugin("gg.Mod.Pack")]
+[BepInProcess("Among Us.exe")]
+[BepInDependency(ReactorPlugin.Id)]
+[ReactorModFlags(ModFlags.RequireOnAllClients)]
+public partial class ModPackPlugin : BasePlugin
+{
+    private static StringNames _helloStringName;
+
+    public override void Load()
+    {
+        ReactorCredits.Register<ModPackPlugin>(ReactorCredits.AlwaysShow);
+
+        this.AddComponent<ModPackComponent>();
+
+        _helloStringName = CustomStringName.CreateAndRegister("Hello!");
+        LocalizationManager.Register(new ExampleLocalizationProvider());
+    }
+
+    [RegisterInIl2Cpp]
+    public class ModPackComponent : MonoBehaviour
+    {
+        [HideFromIl2Cpp]
+        public DragWindow TestWindow { get; }
+
+        public ModPackComponent(IntPtr ptr) : base(ptr)
+        {
+            TestWindow = new DragWindow(new Rect(60, 20, 0, 0), "ModPack", () =>
+            {
+                if (GUILayout.Button("Log CustomStringName"))
+                {
+                    Logger<ModPackPlugin>.Info(TranslationController.Instance.GetString(_helloStringName));
+                }
+
+                if (GUILayout.Button("Log localized string"))
+                {
+                    Logger<ModPackPlugin>.Info(TranslationController.Instance.GetString((StringNames) 1337));
+                }
+
+                if (AmongUsClient.Instance && PlayerControl.LocalPlayer)
+                {
+                    if (GUILayout.Button("Send ModPackRpc"))
+                    {
+                        var playerName = PlayerControl.LocalPlayer.Data.PlayerName;
+                        Rpc<ModPackRpc>.Instance.Send(new ModPackRpc.Data($"Send: from {playerName}"), ackCallback: () =>
+                        {
+                            Logger<ModPackPlugin>.Info("Got an acknowledgement for modpack rpc");
+                        });
+
+                        if (!AmongUsClient.Instance.AmHost)
+                        {
+                            Rpc<ModPackRpc>.Instance.SendTo(AmongUsClient.Instance.HostId, new ModPackRpc.Data($"SendTo: from {playerName} to host"));
+                        }
+                    }
+
+                    if (GUILayout.Button("Send MethodModPackRpc"))
+                    {
+                        RpcSay(PlayerControl.LocalPlayer, "Hello from method rpc", Random.value, PlayerControl.LocalPlayer);
+                    }
+                }
+            })
+            {
+                Enabled = false,
+            };
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F2))
+            {
+                TestWindow.Enabled = !TestWindow.Enabled;
+            }
+        }
+
+        private void OnGUI()
+        {
+            TestWindow.OnGUI();
+        }
+    }
+
+    [MethodRpc((uint) CustomRpcCalls.MethodRpcExample)]
+    public static void RpcSay(PlayerControl player, string text, float number, PlayerControl testPlayer)
+    {
+        Logger<ModPackPlugin>.Info($"{player.Data.PlayerName} text: {text} number: {number} testPlayer: {testPlayer.NetId}");
+    }
+}
